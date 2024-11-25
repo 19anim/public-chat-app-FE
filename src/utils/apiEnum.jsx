74cc008052879ff569a88729ee1b52c6f943ecc8
19.anim <nptatdt@gmail.com>
2024-11-26 00:23:58 +0700

    add proxy

--- src/utils/apiEnum.jsx
+++ src/utils/apiEnum.jsx
@@ -2,7 +2,9 @@ import ENV from "./envEnum";
 
 const environment = import.meta.env.VITE_ENVIRONMENT || VITE_ENVIRONMENT;
 const baseAPI =
-  environment === ENV.DEV ? "/api/" : "https://public-chat-app-be.onrender.com";
+  environment === ENV.DEV
+    ? "/api/"
+    : "https://public-chat-app-be.onrender.com/";
 
 const API = {
   LOGIN: "auth/signin",
